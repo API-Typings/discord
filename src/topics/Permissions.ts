a15edce9e028d4dbd20c588bfc4b300776e97553
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-06 01:24:16 -0400

    fix(Permissions): use type union for `Permission`
    Recursive type was causing a crash during build; temporary(?) fix

--- src/topics/Permissions.ts
+++ src/topics/Permissions.ts
@@ -1,4 +1,3 @@
-import type { CamelCase, Delimited } from 'extended-utility-types';
 import type { Snowflake } from '../';
 
 // ANCHOR Permission Flags
@@ -212,7 +211,39 @@ export enum PermissionFlags {
 	UseSlashCommands = 0x80000000
 }
 
-export type Permission = Uppercase<Delimited<CamelCase<keyof typeof PermissionFlags>, '_'>>;
+export type Permission =
+	| 'CREATE_INSTANT_INVITE'
+	| 'KICK_MEMBERS'
+	| 'BAN_MEMBERS'
+	| 'ADMINISTRATOR'
+	| 'MANAGE_CHANNELS'
+	| 'MANAGE_GUILD'
+	| 'ADD_REACTIONS'
+	| 'VIEW_AUDIT_LOG'
+	| 'PRIORITY_SPEAKER'
+	| 'STREAM'
+	| 'VIEW_CHANNEL'
+	| 'SEND_MESSAGES'
+	| 'SEND_TTS_MESSAGES'
+	| 'MANAGE_MESSAGES'
+	| 'EMBED_LINKS'
+	| 'ATTACH_FILES'
+	| 'READ_MESSAGE_HISTORY'
+	| 'MENTION_EVERYONE'
+	| 'USE_EXTERNAL_EMOJIS'
+	| 'VIEW_GUILD_INSIGHTS'
+	| 'CONNECT'
+	| 'SPEAK'
+	| 'MUTE_MEMBERS'
+	| 'DEAFEN_MEMBERS'
+	| 'MOVE_MEMBERS'
+	| 'USE_VAD'
+	| 'CHANGE_NICKNAME'
+	| 'MANAGE_NICKNAMES'
+	| 'MANAGE_ROLES'
+	| 'MANAGE_WEBHOOKS'
+	| 'MANAGE_EMOJIS'
+	| 'USE_SLASH_COMMANDS';
 
 // SECTION Roles
 
