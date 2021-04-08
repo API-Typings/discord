34f0ce830db69de954261ae6be2e1679f9657247
K. Akatsuki <thevortexspirit@gmail.com>
2021-04-07 21:11:53 -0400

    revert(Permissions): use original `Permission` type

--- src/topics/Permissions.ts
+++ src/topics/Permissions.ts
@@ -1,3 +1,4 @@
+import { CamelCase, Delimit } from 'extended-utility-types';
 import type { Snowflake } from '../';
 
 // ANCHOR Permission Flags
@@ -216,39 +217,7 @@ export enum PermissionFlags {
 	RequestToSpeak = 0x100000000
 }
 
-export type Permission =
-	| 'CREATE_INSTANT_INVITE'
-	| 'KICK_MEMBERS'
-	| 'BAN_MEMBERS'
-	| 'ADMINISTRATOR'
-	| 'MANAGE_CHANNELS'
-	| 'MANAGE_GUILD'
-	| 'ADD_REACTIONS'
-	| 'VIEW_AUDIT_LOG'
-	| 'PRIORITY_SPEAKER'
-	| 'STREAM'
-	| 'VIEW_CHANNEL'
-	| 'SEND_MESSAGES'
-	| 'SEND_TTS_MESSAGES'
-	| 'MANAGE_MESSAGES'
-	| 'EMBED_LINKS'
-	| 'ATTACH_FILES'
-	| 'READ_MESSAGE_HISTORY'
-	| 'MENTION_EVERYONE'
-	| 'USE_EXTERNAL_EMOJIS'
-	| 'VIEW_GUILD_INSIGHTS'
-	| 'CONNECT'
-	| 'SPEAK'
-	| 'MUTE_MEMBERS'
-	| 'DEAFEN_MEMBERS'
-	| 'MOVE_MEMBERS'
-	| 'USE_VAD'
-	| 'CHANGE_NICKNAME'
-	| 'MANAGE_NICKNAMES'
-	| 'MANAGE_ROLES'
-	| 'MANAGE_WEBHOOKS'
-	| 'MANAGE_EMOJIS'
-	| 'USE_SLASH_COMMANDS';
+export type Permission = Uppercase<Delimit<CamelCase<keyof typeof PermissionFlags>, '_'>>;
 
 // SECTION Roles
 
