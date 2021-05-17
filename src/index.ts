7b29a411ecff38d7323782533b4545f75f5ef77a
K. Akatsuki <thevortexspirit@gmail.com>
2021-05-17 16:16:55 -0400

    feat: `ErrorResponse`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import type { DeepReadonly } from 'extended-utility-types';
+import type { JSONErrorCode } from './';
+
 export * from './interactions';
 export * from './resources';
 export * from './topics';
@@ -21,6 +24,30 @@ export enum BaseURL {
 	 */
 	CDN = 'https://cdn.discordapp.com'
 }
+
+/**
+ * Error responses will specify which JSON key contains the error, the error code, and a human
+ * readable error message.
+ *
+ * @source {@link https://discord.com/developers/docs/reference#error-messages|Reference}
+ */
+export interface ErrorResponse {
+	readonly code: JSONErrorCode;
+	readonly errors?: ArrayError | ObjectError;
+	readonly message: string;
+}
+
+/**
+ * @source {@link https://discord.com/developers/docs/reference#error-messages-array-error|Reference}
+ */
+export type ArrayError = { readonly [index: string]: ObjectError };
+
+/**
+ * @source {@link https://discord.com/developers/docs/reference#error-messages-object-error|Reference}
+ */
+export type ObjectError = DeepReadonly<{
+	[field: string]: { _errors: { code: string; message: string }[] };
+}>;
 
 export enum ClientReleaseChannel {
 	Stable = 'stable',
